9600338766d2164680e80291066d2efa0bcef378
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-05-09 18:09:07 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -29,7 +29,7 @@ async def start(pk, update):
 async def id(pk, send):
  
    await send.reply_text(
-    text='here is your id' + command.chat_id
+    text='here is your id' + send.chat_id
    )
 
 
