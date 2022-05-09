fcde1596373b203a1d48e153434fe1afb395a78f
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-05-09 21:07:31 +0530

    Create BUTTONS.py

---
+++ BUTTONS.py
@@ -0,0 +1,16 @@
+START_BUTTON = InlineKeyboardMarkup(
+    [
+      [
+        InlineKeyboardButton('Menu', callback_data='menu')
+      ]
+    ]
+)
+MENU_BUTTON = InlineKeyboardMarkup(
+    [
+      [
+         InlineKeyboardButton('About', callback_data='about'),
+         InlineKeyboardButton('Help', callback_data='help'),
+         InlineKeyboardButton('Close', callback_data='close')
+      ]
+    ]
+)
