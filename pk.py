b3cc506d455af916defa1d93a0d25596ac5b50f1
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-05-10 09:49:05 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -39,50 +39,61 @@ async def id(pk, message):
    await message.reply_text(
     text=f""" here is your chat id ` {message.from_user.id} `"""
    )
-@Pk.on_message(filters.private & filters.media)
-async def getmedia(pk, update):  
-   medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
-   try:
-      message = await update.reply_text(
-       text="Downloading...",
-       quote=True,
-       disable_web_page_preview=True
+@Pk.on_message(filters.photo)
+async def uploadphoto(client, message):
+  medianame = DOWNLOAD_LOCATION + str(message.chat.id) + ".jpg"
+  try:
+      message = await message.reply_text(
+         text="Downloading...",
+         quote=True,
+         disable_web_page_preview=True
       )
-      await pk.download_media(
-       message=update,
-       file_name=medianame
+      await client.download_media(
+          message="Downloaded successfully ", 
+          file_name=medianame
       )
-      response = upload_file(medianame)
-      try: 
-           os.remove(medianame)
-      except:
-          pass
-   except Exception as error:
-        text=f"Error :- <code>{error}</code>"
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton('More Help', callback_data='help')]]
-        )
-        await message.edit_text(
-            text=text,
-            disable_web_page_preview=True,
-            reply_markup=reply_markup
-        )
-        return
-        text=f"**Link :-** `https://telegra.ph{response[0]}`"
-        reply_markup=InlineKeyboardMarkup(
-            [
-               [
-                   InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
-                   InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
-               ]
-           ]
-        )
-        
-        await message.edit_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True
-        )
+       await message.edit_text("Trying to Upload on Telegraph")
+  try:
+    tlink = upload_file(medianame)
+  except:
+    await message.edit_text("Something went wrong") 
+  else:
+    await message.edit_text(f"https://telegra.ph{tlink[0]}")     
+    os.remove(medianame) 
+
+@Pk.on_message(filters.animation)
+async def uploadgif(client, message):
+  if(message.animation.file_size < 5242880):
+    message = await message.reply_text("Trying to Download")
+    userid = str(message.chat.id)
+    gif_path = (f"./DOWNLOADS/{userid}.mp4")
+    gif_path = await client.download_media(message=message, file_name=gif_path)
+    await message.edit_text("Trying to Upload...")
+    try:
+      tlink = upload_file(gif_path)
+      await message.edit_text(f"https://telegra.ph{tlink[0]}")   
+      os.remove(gif_path)   
+    except:
+      await message.edit_text("Something really Happend Wrong...") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb")
+
+@Pk.on_message(filters.video)
+async def uploadvid(client, message):
+  if(message.video.file_size < 5242880):
+    message = await message.reply_text("Trying to Download...")
+    userid = str(message.chat.id)
+    vid_path = (f"./DOWNLOADS/{userid}.mp4")
+    vid_path = await client.download_media(message=message, file_name=vid_path)
+    await message.edit_text("`Trying to Upload...")
+    try:
+      tlink = upload_file(vid_path)
+      await message.edit_text(f"https://telegra.ph{tlink[0]}")     
+      os.remove(vid_path)   
+    except:
+      await message.edit_text("Something really Happend Wrong...") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb")
 @Pk.on_callback_query()
 async def cb_data(bot, update):
     if update.data == 'about':
