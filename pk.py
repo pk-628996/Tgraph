ec2eefbba2baf46883eab8741cf41aecae66ec2d
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-05-10 10:27:41 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -57,9 +57,9 @@ async def uploadphoto(client, message):
          tlink = upload_file(medianame)
       except:
          await message.edit_text("Something went wrong") 
-  else:
-    await message.edit_text(f"https://telegra.ph{tlink[0]}")     
-    os.remove(medianame) 
+   else:
+     await message.edit_text(f"https://telegra.ph{tlink[0]}")     
+     os.remove(medianame) 
 
 @Pk.on_message(filters.animation)
 async def uploadgif(client, message):
