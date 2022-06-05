1ce1ea9b036b7105be2e668d296125c94287ddb2
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-06-05 21:08:51 +0530

    Update Text.py

--- Text.py
+++ Text.py
@@ -1,5 +1,4 @@
 START_TEXT = """Hii {}, \n
-I am Telegraph bot that doesn't Upload anything to Telegraph.\n
-Under Development."""
-HELP_TEXT = """This text will help you not to use this bot"""
+I am Telegraph bot that can Upload media files upto 5 mb to Telegraph."""
+HELP_TEXT = """You need help? \n\n There is nothing complex in this bot just send the media to be uploaded on Telegraph"""
 
