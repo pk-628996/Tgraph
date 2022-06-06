1407823cae874cca76fa317f078a74fafd7dfe52
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-06-06 20:32:20 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -77,7 +77,7 @@ async def uploadp(pk, message):
 
 @Bot.on_message(filters.photo)
 async def upl(pk, message):
-   await message.reply(text="What I need to do?", reply_markup=InlineKeybordMarkup( [ [ InlineKeyboardButton('Upload',callback_data='uppl') ] ] )
+   await message.reply(text="What I need to do?", reply_markup=InlineKeybordMarkup( [ [ InlineKeyboardButton('Upload',callback_data='uppl') ] ] ))
 
 
 
