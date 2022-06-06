90ef0025ec2e620d22a1f425dbe289344ab8a62b
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-06-06 20:19:31 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -151,10 +151,10 @@ async def cb_data(bot, update):
              tlink = upload_file(file)[0]
              os.remove(file)
           
-             await answer_update(update, 
+      await answer_update(update, 
                f"https://telegra.ph{tlink} \n\n`https://telegra.ph{tlink}` \n\nTap the link to copy " , 
                disable_web_page_preview=True
-             ),
+      ),
             
     elif update.data == 'help':
       await update.message.edit_text(
