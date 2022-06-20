462e090bbce89abc52018d5deaa2d2623df05343
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-06-20 14:16:14 +0530

    Update Text.py

--- Text.py
+++ Text.py
@@ -1,4 +1,4 @@
 START_TEXT = """Hii {},\n
 I am Telegraph bot that can Upload media files upto 5 mb to Telegraph."""
-HELP_TEXT = """You need help? \n\nThere is nothing complex in this bot just send the media to be uploaded on Telegraph."""
+HELP_TEXT = """You need help? \n\nThere is nothing complex in this bot just send the media to be uploaded on Telegraph. To get your chat id  use the command /id. If you are communicating with bot in group still you can use command /id to get chat id. If you want to get id of another person of group then forward a message from that person/group to bot and then reply that message with command /get_id. In order to get chat id of a channel You will have to Use the Command /get_c_id as areply to forwarded message to get the chat id of that channel. """
 
