3e6bafa431a71486599c26653a6bae7467f3b865
pk-628996 <100488688+pk-628996@users.noreply.github.com>
2022-07-18 19:50:43 +0530

    Update pk.py

--- pk.py
+++ pk.py
@@ -17,12 +17,12 @@ Pk = Client(
 
 ABOUT_TEXT="""This Bot can upload photos and other medias upto 5mb to telegraph"""
 @Pk
-async def sjsjjfbf(Client):
-   await Client.send_message(chat_id="5451002182",text="I am started")
+async def sjsjjfbf(Message):
+   await Message.reply(chat_id="5451002182",text="I am started")
 import asyncio 
 import os
 loop = asyncio.get_event_loop()
-loop.run_until_complete(sjsjjfbf(Pk))
+loop.run_until_complete(sjsjjfbf(Message))
 loop.close()
 
   
